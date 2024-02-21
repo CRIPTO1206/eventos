515b090b1399f481834b9aa7652492215bb34e24
Cripto <jhonjairo.alvarezreyes@gmail.com>
2024-02-21 15:19:23 -0500

    Ajustes presentación Swagger

--- src/main/java/com/eventos/controller/EventoController.java
+++ src/main/java/com/eventos/controller/EventoController.java
@@ -2,6 +2,7 @@ package com.eventos.controller;
 
 import com.eventos.entity.Evento;
 import com.eventos.service.EventoService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,30 +10,52 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(path = "eventos")
+@RequestMapping
 public class EventoController {
 
     @Autowired
     private EventoService eventoService;
 
+    @Operation(summary = "Consulta de todos los eventos")
     @GetMapping("/eventos")
     public List<Evento> getAll(){
-        return eventoService.getEventos();
+
+        List<Evento> eventos;
+
+        long startTime = System.nanoTime();
+
+        eventos = eventoService.getEventos();
+
+        long endTime = System.nanoTime();
+
+        long timeElapsed = endTime - startTime;
+
+        System.out.println("Tiempo de ejecución en milisegundos: " + timeElapsed / 1000000);
+
+        return eventos;
+
     }
 
+    @Operation(summary = "Consulta de 1 eventos")
     @GetMapping("/{eventoId}")
     public Optional<Evento> getById(@PathVariable("eventoId") Long eventoId){
         return eventoService.getEvento(eventoId);
     }
 
+    @Operation(summary = "Creación y/o actualización de 1 evento")
     @PostMapping
     public void SaveUpdate(@RequestBody Evento evento){
         eventoService.saveOrUpdate(evento);
     }
 
+    @Operation(summary = "Eliminación de 1 evento")
     @DeleteMapping("/{eventoId}")
     public void delete(@PathVariable("eventoId") Long eventoId){
         eventoService.delete(eventoId);
+    }
+
+    public void geocode(){
+
     }
 
 }
